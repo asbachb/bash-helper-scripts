992bb842255a34f09118c514efcca7829a71fca0
Benjamin Asbach <asbachb@users.noreply.github.com>
2020-01-23 13:47:47 +0100

    Added some aliases for git

---
+++ alias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias gg='git gui'
+alias gp='git pull'
+alias nb='git checkout -b'
+alias gpp='git pull --prune'
+alias rbm='git rebase -i master'
+alias rbc='git rebase --continue'
+alias gm='git checkout master'
